0f6c96ca5f70a3823d189ff483c1713c60530ed5
naveqo <422855+naveqo@users.noreply.github.com>
2020-05-29 19:08:20 +0900

    font-size utility

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -3,11 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <title>Laravel</title>
 
     </head>
     <body>
-    aaa
+    <p>n: あいうえお書きくけこさシス背祖たちつてと何ぬ寝のは皮膚へ歩真美無メモや湯四らりるレロ羽をん</p>
+    <p class="u-fs--s">s: あいうえお書きくけこさシス背祖たちつてと何ぬ寝のは皮膚へ歩真美無メモや湯四らりるレロ羽をん</p>
+    <p class="u-fs--xs">xs: あいうえお書きくけこさシス背祖たちつてと何ぬ寝のは皮膚へ歩真美無メモや湯四らりるレロ羽をん</p>
     </body>
 </html>
