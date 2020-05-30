1d71eec803543f8fdbcbe13eeda421b736d1dfef
naveqo <422855+naveqo@users.noreply.github.com>
2020-05-30 09:55:37 +0900

    フォントサイズ修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -9,6 +9,12 @@
 
     </head>
     <body>
+    <h1 class="c-h1">Heading1 見出し</h1>
+    <h2 class="c-h2">Heading2 見出し</h2>
+    <h3 class="c-h3">Heading3 見出し</h3>
+    <h4 class="c-h4">Heading4 見出し</h4>
+    <h5 class="c-h5">Heading5 見出し</h5>
+    <h6 class="c-h6">Heading6 見出し</h6>
     <p>n: あいうえお書きくけこさシス背祖たちつてと何ぬ寝のは皮膚へ歩真美無メモや湯四らりるレロ羽をん</p>
     <p class="u-fs--s">s: あいうえお書きくけこさシス背祖たちつてと何ぬ寝のは皮膚へ歩真美無メモや湯四らりるレロ羽をん</p>
     <p class="u-fs--xs">xs: あいうえお書きくけこさシス背祖たちつてと何ぬ寝のは皮膚へ歩真美無メモや湯四らりるレロ羽をん</p>
